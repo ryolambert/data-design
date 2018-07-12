ad43b08b1a15dd7436afb21781f8877a7ee22967
James R. Lambert <ryolambert@gmail.com>
2018-07-12 16:32:11 -0600

    added index page for project.

---
+++ epic/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=], initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link rel="stylesheet" type="text/css" href="./style.css"/>
+		<link rel="stylesheet" type="text/css"
+				href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css"
+				href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link href="https://fonts.googleapis.com/css?family=Proza+Libre|Work+Sans" rel="stylesheet">
+		<title>Data Design Index</title>
+
+	</head>
+	<body class="home-page">
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-nav-demo"
+							  aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a href="#" class="navbar-brand"><span class="fa fa-database"></span> Data Design</a>
+				</div>
+				<div class="collapse navbar-collapse" id="bs-nav-demo">
+					<ul class="nav navbar-nav">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="jackDeer.php">Persona</a></li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="use-case.php">Use Case</a></li>
+						<li><a href="conceptual-model.php">Conceptual Model</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="content">
+						<h1>Data-Design Project</h1>
+						<h3>Investigation of Medium's Claps</h3>
+						<hr>
+						<button class="btn btn-default btn-lg" ><i class="fa fa-sign-language"></i> <a href="jackDeer.php">Get Started!</a></button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</body>
+</html>
